3d3b9752356328e7c2e20b8901574d4ee66ebfe0
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2021-03-25 18:51:00 +0000

    Same, but for videosource
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- src/core/videosource.cpp
+++ src/core/videosource.cpp
@@ -717,7 +717,7 @@ void FFMS_VideoSource::DecodeNextFrame(int64_t &AStartTime, int64_t &Pos) {
             return;
         }
     }
-    if (ret == AVERROR_INVALIDDATA) {
+    if (ret == AVERROR(ETIMEDOUT)) {
         char err[1024];
         av_strerror(ret, err, 1024);
         std::string serr(err); // man, c++...
