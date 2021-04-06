4d1fc184b2aeca4b21188457d48697f306dc0092
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2021-04-06 19:39:56 +0100

    Add forgotten EIO to network error list
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -147,6 +147,7 @@ bool IsSamePath(const char *p1, const char *p2) {
 
 bool IsNetworkError(int error) {
     switch (error) {
+    case AVERROR(EIO):
     case AVERROR(ETIMEDOUT):
     case AVERROR(EPROTO):
     case AVERROR(EADDRINUSE):
